81e7838cc3ff725686323be88f6a134bcd944e11
Luke Campbell <luke.s.campbell@gmail.com>
2014-05-19 21:40:49 -0400

    Updated some style a bit and added real front/back

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flashcards.dictionary import Dictionary
 from flask import Flask, render_template
 from config import HOST, PORT
+import random
 
 app = Flask(__name__)
 
@@ -8,10 +9,14 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
-@app.route("/dict/<dictname>")
+@app.route("/dict/random/<dictname>")
 def dictionary(dictname):
     d = Dictionary(dictname)
-    return render_template("echo.html", echo=d.table.keys()[0])
+    random_key = random.choice(d.table.keys())
+
+    front = random_key
+    back = d.table[random_key]['english']
+    return render_template("echo.html", front=front, back=back)
 
 if __name__ == '__main__':
     app.run(host=HOST, port=PORT, debug=True)
--- static/js/flip.js
+++ static/js/flip.js
@@ -1,7 +1,33 @@
 function initFlip() {
-    $("#card").on("click", function(){
-        console.log("hi");
-        $("#card").toggleClass("flipped");
+    $("#card").on("click", flipCard);
+}
+
+function initKeyHandlers() {
+    $(document).keydown(function(e) {
+      switch(e.which) {
+        case 37:
+          console.log("Left");
+          break;
+        case 38:
+          console.log("Up");
+          break;
+        case 39:
+          console.log("Right");
+          location.reload();
+        case 40:
+          console.log("Down");
+          break;
+        case 13:
+          flipCard();
+          break;
+        default: console.log(e.keyCode);
+      }
     });
 }
+
+
+function flipCard() {
+    $("#card").toggleClass("flipped");
+}
+
 
