036f7f1b8ab1cf6ed40aa3fa4721d3fd9178b8ae
Luke Campbell <luke.s.campbell@gmail.com>
2014-05-22 19:57:13 -0400

    Amazing random renders done

--- static/js/flip.js
+++ static/js/flip.js
@@ -13,6 +13,8 @@ function initKeyHandlers() {
           updateRandomCard();
           break;
         case 40: // Down
+          showExamples();
+          showSide();
           break;
         case 13: // Enter
           flipCard();
@@ -39,17 +41,18 @@ function updateRandomCard() {
     }
     var keys = Object.keys(dictionary);
     var choice = keys[Math.floor(keys.length * Math.random())];
-    if (Object.size(dictionary) == 1) {
-        currentKey = choice; // only have one entry so it's not random
-    } else {
+    if (Object.size(dictionary) != 1) {
         while (currentKey === choice) {
             choice = keys[Math.floor(keys.length * Math.random())];
         }
     }
+    currentKey = choice;
 
     var entry = dictionary[choice];
     $(".front").text(choice);
     $(".back").text(entry["english"]);
+    $(".footer-content").empty();
+    $(".side-content").empty();
 }
 
 
@@ -79,4 +82,62 @@ Object.size = function(obj) {
     }
     return size;
 };
+
+function showSide() {
+    if (dictionary === null) {
+        initDictionary();
+    }
+    var kanji = dictionary[currentKey]["kanji"];
+    $(".side-content").text(parseFuri(kanji));
+}
+
+function showExamples() {
+    if (dictionary === null) {
+        initDictionary();
+    }
+    $(".footer-content").empty();
+    var examples = dictionary[currentKey]["examples"];
+    for(var i in examples) {
+        var key = parseFuri(examples[i][0]);
+        var value = examples[i][1];
+        $(".footer-content").append('<div class="top-part"></div>')
+        $(".top-part").append(key);
+        $(".footer-content").append('<div class="bot-part"></div>')
+        $(".bot-part").append(value);
+    }
+}
+
+function parseFuri(inputString) {
+    if(inputString.length <= 0) {
+        return;
+    }
+    /*
+     * First occurrence of the \f escape
+     */
+    var esc = inputString.indexOf("{");
+    if(esc == -1) {
+        return inputString;
+    }
+    // find the end of the }
+    var endPart = inputString.indexOf("}", esc+1);
+    if(endPart == -1) {
+        return inputString;
+    }
+
+    /* right off the bat this part up until the \f
+     * warrants a <ruby> */
+
+    var retval = inputString.substring(0, esc);
+    retval += "<ruby><rb>" + inputString.substring(esc+1, endPart) + "</rb>";
+
+    /* Check if there's another {} */
+    var secondEnd = inputString.indexOf("}", endPart+1);
+    if((inputString[endPart+1] == '{') && (secondEnd != -1)) {
+        retval += "<rt>" + inputString.substring(endPart+2, secondEnd) + "</rt>";
+    }
+    retval += "</ruby>";
+    retval += parseFuri(inputString.substring(secondEnd+1));
+    return retval;
+}
+
 
