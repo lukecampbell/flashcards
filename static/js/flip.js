8f3ca2dd921575952791acc5d6f06b2af2cd1d06
Luke Campbell <luke.s.campbell@gmail.com>
2014-05-23 12:58:03 -0400

    Better docuemntation for flip.js

--- static/js/flip.js
+++ static/js/flip.js
@@ -1,7 +1,22 @@
+/*
+ * flip.js
+ *
+ * Content control and view-controller for the random dictionary portion of flashcards
+ */
+
+/* Adds the click event handler for clicking in the card's context/frame */
 function initFlip() {
     $("#card").on("click", flipCard);
 }
 
+/* Initializes keyboard handlers for this view
+ *
+ * Up: resets the side-bar and bottom content
+ * Right: new card
+ * Down: Shows bottom and side-bar content
+ * Enter: Flips the card
+ * Space: Flips the card and shows bottom content
+ */
 function initKeyHandlers() {
     $(document).keydown(function(e) {
       switch(e.which) {
@@ -34,6 +49,9 @@ function initKeyHandlers() {
 var dictionary = null; // The loaded dictionary
 var currentKey = null; // Global to ensure we don't repeat cards
 
+/* Initializes the dictionary for this view/controller
+ * Note: should only be called once per page load
+ */
 function initDictionary() {
     var dictName = getDictName();
     getDictionary(dictName, function(dict) {
@@ -47,10 +65,13 @@ function updateTitle(newTitle) {
     $("title").text(newTitle);
 }
 
+/* Picks a <different> random entry from the dictionary to display */
 function updateRandomCard() {
+    
     if (dictionary === null)  {
         return initDictionary();
     }
+    // Pick a random entry
     var keys = Object.keys(dictionary);
     var choice = keys[Math.floor(keys.length * Math.random())];
     if (Object.size(dictionary) != 1) {
@@ -60,6 +81,7 @@ function updateRandomCard() {
     }
     currentKey = choice;
 
+    // Update the content
     var entry = dictionary[choice];
     $(".front").text(choice);
     $(".back").text(entry["english"]);
@@ -81,12 +103,14 @@ function getDictionary(dictName,success,failure) {
 }
 
 
+/* Determines the dictionary name using the URL string */
 function getDictName() {
     var ref = window.location.toString();
     var name = ref.substring(ref.lastIndexOf('/')+1);
     return name;
 }
 
+/* From stack overflow, length of associative array */
 Object.size = function(obj) {
     var size = 0, key;
     for (key in obj) {
@@ -95,6 +119,9 @@ Object.size = function(obj) {
     return size;
 };
 
+/* Displays the side content
+ * Note: comes from the "yomi" in the dictionary
+ */
 function showSide() {
     if (dictionary === null) {
         initDictionary();
@@ -104,6 +131,9 @@ function showSide() {
     $(".side-content").append(parseFuri(yomi));
 }
 
+/*
+ * Shows the bottom content, which comes from the "examples"
+ */
 function showExamples() {
     if (dictionary === null) {
         initDictionary();
@@ -120,26 +150,33 @@ function showExamples() {
     }
 }
 
+/*
+ * Parses furigana escapes in dictionary entries.
+ * Surrounding string entries in {bottom}{furigana} causes appropriate HTML
+ * ruby tags to be generated for the entry.
+ */
 function parseFuri(inputString) {
     if(inputString.length <= 0) {
         return "";
     }
     /*
-     * First occurrence of the \f escape
+     * First occurrence of the { escape
      */
     var esc = inputString.indexOf("{");
     if(esc == -1) {
         return inputString;
     }
-    // find the end of the }
+    /*
+     * Get the end of the escaped section
+     */
     var endPart = inputString.indexOf("}", esc+1);
     if(endPart == -1) {
         return inputString;
     }
 
-    /* right off the bat this part up until the \f
-     * warrants a <ruby> */
-
+    /*
+     * Surround what we have so far in a ruby tag
+     */
     var retval = inputString.substring(0, esc);
     retval += "<ruby><rb>" + inputString.substring(esc+1, endPart) + "</rb>";
 
@@ -149,6 +186,7 @@ function parseFuri(inputString) {
         retval += "<rt>" + inputString.substring(endPart+2, secondEnd) + "</rt>";
     }
     retval += "</ruby>";
+    /* Recursion makes my life so much easier here */
     retval += parseFuri(inputString.substring(secondEnd+1));
     return retval;
 }
