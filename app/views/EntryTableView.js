ca8a7e3534bcc3fb900d1cfc5cbe7085dde17d71
Luke Campbell <luke.s.campbell@gmail.com>
2015-05-10 12:22:25 -0400

    Adds a filter box

--- app/views/EntryTableView.js
+++ app/views/EntryTableView.js
@@ -4,6 +4,7 @@ var EntryTableView = Backbone.View.extend({
   },
   initialize: function() {
     var self = this;
+    _.bindAll(this, 'onSelect', 'onFilter');
     this.initialRender();
     this.listenTo(this.collection, "reset", function() {
       self.render();
@@ -11,6 +12,7 @@ var EntryTableView = Backbone.View.extend({
     this.listenTo(this.collection, 'change', function() {
       self.render();
     });
+    this.listenTo(app, 'app:filter', this.onFilter);
   },
   initialRender: function() {
     this.$el.html();
@@ -21,9 +23,29 @@ var EntryTableView = Backbone.View.extend({
     var model = this.collection.get(model_id);
     app.trigger('app:edit', model);
   },
+  onFilter: function(e) {
+    var value = $(e.target).val();
+    if(value === "") {
+      return this.render();
+    }
+    var filteredCollection = this.collection.filter(function(item) {
+      if(item.get('title').indexOf(value) >= 0) {
+        return true;
+      }
+      if(item.get('english').indexOf(value) >= 0) {
+        return true;
+      }
+      if(item.get('yomi').indexOf(value) >= 0) {
+        return true;
+      }
+      return false;
+    });
+    this.$el.html(this.template({collection: new EntryCollection(filteredCollection)}));
+    return this;
+  },
   template: JST['app/partials/EntryTable.html'],
   render: function() {
-    console.log('render');
     this.$el.html(this.template({collection: this.collection}));
+    return this;
   }
 });
